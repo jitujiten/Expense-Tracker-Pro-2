1939e1c2ab620d8591d3440cb748250b8ec48f77
jitujiten <sahuohjitu@gmail.com>
2023-02-16 14:48:37 +0530

    Added a emailverify button

--- src/Component/Profile/ExpensePage.js
+++ src/Component/Profile/ExpensePage.js
@@ -1,12 +1,64 @@
-import React from "react";
+import React,{useContext} from "react";
 import PageHeader from "./PageHeader";
-
+import classes from "./ExpensePage.module.css";
+import AuthContext from "../Context/AuthContext";
 
 function ExpensePage() {
+
+const authCtx =useContext(AuthContext);
+
+
+
+const submitHandler=(e)=>{
+  e.preventDefault();
+  
+
+  
+fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyD5Ls-KCDVH0n1SoRjuqDNCBEmY10N3zaY',{
+  method:'POST',
+  body:JSON.stringify({
+    requestType:"VERIFY_EMAIL",
+    idToken:authCtx.tokenid,
+  }),
+  headers:{
+    'Content-Type': "application/json"
+  },
+}).then((res)=>{
+  if(res.ok){
+    return  res.json();
+  }else{
+    return res.json().then((data)=>{
+       let errormessage = 'Authentication failed'
+       if(data && data.error&& data.error.message){
+        errormessage = data.error.message;
+       }
+       throw new Error(errormessage)
+    })
+  }
+}).then((data)=>{
+  console.log(data)
+  alert('success')
+
+}).catch((err)=>{
+  console.log(err)
+  alert(err.message)
+})
+}
+
+
+
+
   return (
     <React.Fragment>
-     <PageHeader/>
+      <PageHeader />
       <hr></hr>
+      <form onSubmit={submitHandler}>
+        <div className={classes.expensepage}>
+          {" "}
+          <label htmlFor="emailverification">Please Verify Your Email..</label>
+          <button className={classes.but}>Email Verificatin</button>
+        </div>
+      </form>
     </React.Fragment>
   );
 }
